190fb7979d3de6006c1c7342ecd5333b4f47674c
Jon Noriega <noriega331@gmail.com>
2020-08-19 14:59:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,10 @@
-def find_even_values(src)
-  
-  array_of_arrays = [
+array_of_arrays = [
     [10,11],
     [99,50,3,4],
     [23,41]
     ]
+
+def find_even_values(src)
   
   row_index = 0
   evens_array = []
